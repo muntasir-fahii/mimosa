af3ec38e88c4381a84cffda6b9c072e2a46a0e49
muntasir-fahii <fa3877957@gmail.com>
2023-08-29 01:28:35 +0600

    hero done

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,15 +1,18 @@
+import { cn } from "@/lib/utils";
+import { Playfair_Display } from "next/font/google";
 import Image from "next/image";
-import React from "react";
+
+const playFairDisplay = Playfair_Display({ subsets: ["latin"] });
 
 const Hero = () => {
   return (
     <section className="wrapper section-padding h-[calc(100vh-5rem)] ">
-      <div className="w-full h-full grid grid-rows-8 gap-5 bg-red">
+      <div className="w-full h-full grid grid-rows-8 gap-5 ">
         {/* first row */}
         <div className="w-full h-[8rem] grid grid-cols-[30%_auto] gap-20 ">
           <div className="w-full h-full overflow-hidden rounded-[0px_50px_0px_50px]">
             <Image
-              src="https://images.pexels.com/photos/799420/pexels-photo-799420.jpeg?auto=compress&cs=tinysrgb&w=600"
+              src="https://images.pexels.com/photos/823816/pexels-photo-823816.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt="Photography of a Woman Holding Lights
 "
               width={640}
@@ -18,21 +21,26 @@ const Hero = () => {
             />
           </div>
           <div className="w-full h-full flex items-center">
-            <h2 className="text-8xl uppercase font-semibold">
-              lorem ipsum ipsum
-            </h2>
+            <h2 className="text-8xl uppercase font-bold">Discover New</h2>
           </div>
         </div>
         <hr className="border-gray" />
         {/* second row */}
         <div className="w-full h-[8rem] grid grid-cols-[25%_auto_50%] gap-20">
           <div className="w-full h-full flex items-center">
-            <h2 className="text-8xl uppercase font-semibold">lorem</h2>
+            <h2
+              className={cn(
+                playFairDisplay.className,
+                "text-8xl lowercase font-normal"
+              )}
+            >
+              Realms<span className="text-blue">.</span>
+            </h2>
           </div>
           <div className="w-full h-full overflow-hidden rounded-[0px_50px_0px_50px]">
             <Image
-              src="https://images.pexels.com/photos/799420/pexels-photo-799420.jpeg?auto=compress&cs=tinysrgb&w=600"
-              alt="Photography of a Woman Holding Lights
+              src="https://images.pexels.com/photos/839633/pexels-photo-839633.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="Women's Red Lips
 "
               width={320}
               height={180}
@@ -40,19 +48,20 @@ const Hero = () => {
             />
           </div>
           <div className="w-full h-full flex items-center">
-            <h2 className="text-8xl uppercase font-semibold">lorems lorem</h2>
+            <h2 className="text-8xl uppercase font-bold">Explore Your</h2>
           </div>
         </div>
         <hr className="border-gray" />
         {/* third row */}
         <div className="w-full h-[8rem] grid grid-cols-[40%_auto] gap-20">
           <div className="w-full h-full flex items-center">
-            <h2 className="text-8xl uppercase font-semibold">lorems los</h2>
+            <h2 className="text-8xl uppercase font-bold">Goddess</h2>
           </div>
           <div className="w-full h-full overflow-hidden rounded-[0px_50px_0px_50px]">
             <Image
-              src="https://images.pexels.com/photos/799420/pexels-photo-799420.jpeg?auto=compress&cs=tinysrgb&w=600"
-              alt="Photography of a Woman Holding Lights
+              src="https://images.pexels.com/photos/3294253/pexels-photo-3294253.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="Close-Up Photo Of Woman
+
 "
               width={1280}
               height={720}
@@ -65,7 +74,7 @@ const Hero = () => {
         <div className="w-full h-[8rem] grid grid-cols-[35%_auto] gap-20">
           <div className="w-full h-full overflow-hidden rounded-[0px_50px_0px_50px]">
             <Image
-              src="https://images.pexels.com/photos/799420/pexels-photo-799420.jpeg?auto=compress&cs=tinysrgb&w=600"
+              src="https://images.pexels.com/photos/8221203/pexels-photo-8221203.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt="Photography of a Woman Holding Lights
 "
               width={1280}
@@ -74,7 +83,27 @@ const Hero = () => {
             />
           </div>
           <div className="w-full h-full flex items-center">
-            <h2 className="text-8xl uppercase font-semibold">lorems los la</h2>
+            <h2 className="text-8xl uppercase font-bold">
+              <span
+                className={cn(
+                  playFairDisplay.className,
+                  "lowercase font-normal"
+                )}
+              >
+                within
+              </span>{" "}
+              M
+              <span
+                className={cn(
+                  playFairDisplay.className,
+                  "text-orange font-normal lowercase"
+                )}
+              >
+                i
+              </span>
+              mosa
+              <span className="text-pink">.</span>
+            </h2>
           </div>
         </div>
         <hr className="border-gray" />
