7343c9dde8ad84e0356e505f6bdaa1e803c6bb45
muntasir-fahii <fa3877957@gmail.com>
2023-09-01 15:53:21 +0600

    hero btn added

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,13 +1,14 @@
 import { cn } from "@/lib/utils";
 import { Playfair_Display } from "next/font/google";
 import Image from "next/image";
+import Link from "next/link";
 
 const playFairDisplay = Playfair_Display({ subsets: ["latin"] });
 
 const Hero = () => {
   return (
     <section className="wrapper section-padding h-[calc(100vh-5rem)] ">
-      <div className="w-full h-full grid grid-rows-8 gap-5 ">
+      <div className="w-full h-full grid grid-rows-8 gap-5 relative">
         {/* first row */}
         <div className="w-full h-[8rem] grid grid-cols-[30%_auto] gap-20 ">
           <div className="w-full h-full overflow-hidden rounded-[0px_50px_0px_50px]">
@@ -21,23 +22,25 @@ const Hero = () => {
             />
           </div>
           <div className="w-full h-full flex items-center">
-            <h2 className="text-8xl uppercase font-bold">Discover New</h2>
+            <h2 className="headline-1 uppercase font-bold">Discover New</h2>
           </div>
         </div>
+
         <hr className="border-gray" />
+
         {/* second row */}
         <div className="w-full h-[8rem] grid grid-cols-[25%_auto_50%] gap-20">
           <div className="w-full h-full flex items-center">
             <h2
               className={cn(
                 playFairDisplay.className,
-                "text-8xl lowercase font-normal"
+                "headline-1 lowercase font-normal"
               )}
             >
               Realms<span className="text-blue">.</span>
             </h2>
           </div>
-          <div className="w-full h-full overflow-hidden rounded-[0px_50px_0px_50px]">
+          <div className="w-full h-full overflow-hidden rounded-[50px_0px_50px_50px]">
             <Image
               src="https://images.pexels.com/photos/839633/pexels-photo-839633.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt="Women's Red Lips
@@ -48,14 +51,16 @@ const Hero = () => {
             />
           </div>
           <div className="w-full h-full flex items-center">
-            <h2 className="text-8xl uppercase font-bold">Explore Your</h2>
+            <h2 className="headline-1 uppercase font-bold">Test your </h2>
           </div>
         </div>
+
         <hr className="border-gray" />
+
         {/* third row */}
         <div className="w-full h-[8rem] grid grid-cols-[40%_auto] gap-20">
           <div className="w-full h-full flex items-center">
-            <h2 className="text-8xl uppercase font-bold">Goddess</h2>
+            <h2 className="headline-1 uppercase font-bold">Goddess</h2>
           </div>
           <div className="w-full h-full overflow-hidden rounded-[0px_50px_0px_50px]">
             <Image
@@ -69,7 +74,9 @@ const Hero = () => {
             />
           </div>
         </div>
+
         <hr className="border-gray" />
+
         {/* fourth row */}
         <div className="w-full h-[8rem] grid grid-cols-[35%_auto] gap-20">
           <div className="w-full h-full overflow-hidden rounded-[0px_50px_0px_50px]">
@@ -83,14 +90,14 @@ const Hero = () => {
             />
           </div>
           <div className="w-full h-full flex items-center">
-            <h2 className="text-8xl uppercase font-bold">
+            <h2 className="headline-1 uppercase font-bold">
               <span
                 className={cn(
                   playFairDisplay.className,
                   "lowercase font-normal"
                 )}
               >
-                within
+                with
               </span>{" "}
               M
               <span
@@ -106,7 +113,21 @@ const Hero = () => {
             </h2>
           </div>
         </div>
+
         <hr className="border-gray" />
+
+        {/* packages button */}
+        <Link
+          href="/#packages"
+          className="bg-dark/80 backdrop-blur-lg text-light w-40 h-40 rounded-full overflow-hidden flex justify-center items-center text-lg uppercase absolute left-[35%]  bottom-[13%]  z-[1] group shadow-2xl eq shadow-dark/50 hover:shadow-violet-50"
+        >
+          <p className="z-[3]"> Packages </p>
+
+          {/*  filler-1 */}
+          <span className="absolute z-[2] top-full left-0 bottom-0 right-0 w-full h-1/2 bg-violet group-hover:top-1/2 eq"></span>
+          {/*  filler-2 */}
+          <span className="absolute z-[2] -top-full left-0 bottom-0 right-0 w-full h-1/2 bg-violet group-hover:top-0 eq"></span>
+        </Link>
       </div>
     </section>
   );
