686d674da7083f5c8e28a5843f326fce259d2937
muntasir-fahii <fa3877957@gmail.com>
2023-09-01 16:13:14 +0600

    section Title added

---
+++ components/home/Packages.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SectionTitle from "../shared/SectionTitle";
+
+const Packages = () => {
+  return (
+    <section className="wrapper section-padding" id="packages">
+      <SectionTitle title="Explore our beauty packages" subtitle="Packages" />
+    </section>
+  );
+};
+
+export default Packages;
