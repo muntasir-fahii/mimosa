40731692d2431cd92918b634f51578e0aeaba263
muntasir-fahii <fa3877957@gmail.com>
2023-09-08 23:11:59 +0600

    gallery added

---
+++ components/home/Gallery.tsx
@@ -0,0 +1,43 @@
+import { data } from '@/data/gallery';
+import SectionTitle from '../shared/SectionTitle';
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import { gallery } from '@/types/gallery';
+
+const Gallery = () => {
+  return (
+    <section className='min-h-screen bg-dark text-light'>
+      <div className='wrapper section-padding h-full w-full'>
+        <SectionTitle
+          title='Explore The Glams of Beuaties'
+          subtitle='Gallery'
+          color='bg-pink'
+        />
+
+        {/* Grid Parent */}
+        <div className='gallery-grid-parent h-screen w-full gap-10'>
+          {data.map((item: gallery) => (
+            <div
+              key={item.id}
+              className={cn(
+                item.class,
+                'eq group h-full w-full overflow-hidden border-[12px] border-pink/20 shadow-2xl hover:border-pink/30'
+              )}
+            >
+              <Image
+                src={item.src}
+                alt={item.alt}
+                height={item.height}
+                width={item.width}
+                property=''
+                className='eq h-full w-full object-cover brightness-75 grayscale group-hover:brightness-100 group-hover:grayscale-0'
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Gallery;
